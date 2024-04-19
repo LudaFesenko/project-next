1cc6f25a5ced1f80521b168c2b7fd28066bb9c07
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2024-04-19 12:39:08 +0300

    rework using Tailwind

--- src/app/components/ActiveLabel.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import style from './ActiveLabel.module.css';
-interface Props {
-  children: React.ReactNode;
-}
-
-export default function ActiveLabel({ children }: Props) {
-  return <span className={style.label}>{children}</span>;
-}
--- src/app/components/NotActiveLabel.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import style from './NotActive.module.css';
-interface Props {}
-
-function NotActiveLabel({ children }: PropsWithChildren<Props>) {
-  return <span className={style.label}>{children}</span>;
-}
-
-export default NotActiveLabel;
