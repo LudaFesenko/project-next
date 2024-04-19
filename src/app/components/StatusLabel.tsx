b5f9218c1f7f76dfca9e9fade2760153c2bfc4b0
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2024-04-19 14:21:14 +0300

    use clsx

--- src/app/components/StatusLabel.tsx
+++ src/app/components/StatusLabel.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 
 export enum Status {
@@ -10,18 +11,24 @@ export enum Status {
 interface Props {
   children: React.ReactNode;
   status: Status;
+  disabled?: boolean;
 }
 
-export default function StatusLabel({ children, status }: Props) {
+export default function StatusLabel({ children, status, disabled }: Props) {
   return (
     <div
-      className={`inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium ${
-        (status === Status.Active && 'text-green-700 bg-green-100') ||
-        (status === Status.NotActive && 'text-red-700 bg-red-100') ||
-        (status === Status.Pending && 'text-orange-700 bg-orange-100') ||
-        (status === Status.Suspended && 'text-blue-700 bg-blue-100') ||
-        ''
-      }`}
+      className={clsx(
+        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
+
+        status === Status.Active && 'text-green-700 bg-green-100',
+        status === Status.NotActive && 'text-red-700 bg-red-100',
+        status === Status.Pending && 'text-orange-700 bg-orange-100',
+        status === Status.Suspended && 'text-blue-700 bg-blue-100',
+
+        {
+          ['opacity-75 cursor-not-allowed']: disabled,
+        }
+      )}
     >
       <div className="w-1 h-1 rounded-full mr-2 bg-current" />
       {children}
