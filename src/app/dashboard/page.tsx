fd3e5fe93f2fb52d24fe6348414c52bfe821cab9
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2024-04-24 16:13:15 +0300

    add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {}
+
+export default function Page({}: Props) {
+  return (
+    <main>
+      <div className="text-xl">Dashboard Page</div>
+    </main>
+  );
+}
